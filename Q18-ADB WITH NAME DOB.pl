16c7eb6b044f6a645ef0695a0c48b8b6d09bac26
nandhinikodumuri <123725173+nandhinikodumuri@users.noreply.github.com>
2024-11-26 22:12:59 +0530

    Update and rename Q18-ADB WITH NAME DOB to Q18-ADB WITH NAME DOB.pl

--- Q18-ADB WITH NAME DOB.pl
+++ Q18-ADB WITH NAME DOB.pl
@@ -7,3 +7,13 @@ name(lisa, date(1982, 7, 3)).
 % Find the DOB of a person
 dob_of_person(Name, DOB) :-
     name(Name, DOB)
+output:
+name(Name,DOB).
+e = john,
+DOB = date(1990, 5, 15) ;
+Name = susan,
+DOB = date(1985, 10, 20) ;
+Name = mike,
+DOB = date(1995, 2, 8) ;
+Name = lisa,
+DOB = date(1982, 7, 3).
