95b76ba7324f2e041bb6f040e53b44bafaf72071
nandhinikodumuri <123725173+nandhinikodumuri@users.noreply.github.com>
2024-11-27 08:20:53 +0530

    Create Q26-backtracking.pl

---
+++ Q26-backtracking.pl
@@ -0,0 +1,12 @@
+color(apple, red).
+color(banana, yellow).
+color(orange, orange).
+color(grape, purple).
+color(strawberry, rose).
+color(dragonfruit,pink).
+color(jackfruit,green).
+output:
+ color(apple,Color).
+Color = red.
+?- color(Fruit,pink).
+Fruit = dragonfruit.
